1d955393ec5d454c9d80c0ed881117452ba8de2e
Zachary Sun <sunguohao8895@gmail.com>
2024-01-02 20:07:26 -0500

    Update pretrain.sh

--- pretrain.sh
+++ pretrain.sh
@@ -4,7 +4,7 @@ deepspeed train_mem.py \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version plain \
     --data_path ./mixTraindata/blip_laion_cc_sbu_558k.json \
-    --image_folder ./mixTraindata \
+    --image_folder ./mixTraindata/llava/llava_pretrain/images \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type cluster \
     --tune_mm_mlp_adapter True \
