6b5e0dec244c301876d3a652bf42227c80d13a88
Zachary Sun <sunguohao8895@gmail.com>
2024-03-03 01:33:58 -0500

    Update sophon_lora_gpt4v_clu_sq.sh

--- sophon_lora_gpt4v_clu_sq.sh
+++ sophon_lora_gpt4v_clu_sq.sh
@@ -16,9 +16,9 @@ deepspeed train_mem.py \
     --bf16 True \
     --output_dir ./checkpoints/sqllava-v1.7-13b-lora-gpt4v-vloraPTonly-cluster-sq50 \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 6 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
