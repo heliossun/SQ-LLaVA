ad2003b51da4538fbacb65a3aa787940f52e4849
Zachary Sun <sunguohao8895@gmail.com>
2024-03-17 13:53:33 -0400

    Update captioning.sh

--- scripts/eval/captioning.sh
+++ scripts/eval/captioning.sh
@@ -1,6 +1,6 @@
 
  
-CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
+CUDA_VISIBLE_DEVICES=0 python -m sqllava.eval.model_cap \
     --model_path ./checkpoints/path/to/ckpt/\
     --model_base ./checkpoints/sharegpt4_pretrain \
  	--question-file ./playground/coco/question3.jsonl \
@@ -10,13 +10,13 @@ CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
 	--lora_pretrain ./checkpoints/path/to/ckpt/Vit-lora \
 
 
-   python llava/eval/eval_image_caption.py \
+   python sqllava/eval/eval_image_caption.py \
    	--answers-file ./output/coco/1200.jsonl\
    	--annotation path/to/coco2014/annotations/coco_karpathy_test.json\
    	--model_name "1200" \
    	--dataset "coco"
 
-CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
+CUDA_VISIBLE_DEVICES=0 python -m sqllava.eval.model_cap \
     --model_path ./checkpoints/path/to/ckpt\
     --model_base ./checkpoints/sharegpt4_pretrain \
 	--question-file ./playground/nocaps/near-domain_question.jsonl \
@@ -26,13 +26,13 @@ CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
 	--temperature 0 \
 	--lora_pretrain ./checkpoints/path/to/ckpt/Vit-lora \
 #
-python llava/eval/eval_image_caption.py \
+python sqllava/eval/eval_image_caption.py \
 	--answers-file ./output/nocaps/1200.jsonl\
 	--annotation path/to/nocaps/nocaps_val_near_domain.json\
 	--model_name "1200" \
 	--dataset "nocap_near_d"
 
-CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
+CUDA_VISIBLE_DEVICES=0 python -m sqllava.eval.model_cap \
     --model_path ./checkpoints/path/to/ckpt \
     --model_base ./checkpoints/sharegpt4_pretrain\
 	--question-file ./playground/flickr/question3.jsonl \
@@ -41,14 +41,14 @@ CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
 	--conv-mode="v1" \
 	--lora_pretrain ./checkpoints/path/to/ckpt/Vit-lora \
 
-python llava/eval/eval_image_caption.py \
+python sqllava/eval/eval_image_caption.py \
 	--answers-file ./output/flickr/1200.jsonl\
 	--annotation path/to/Flickr/annotations/flickr30k_test.json\
 	--model_name "1200" \
 	--dataset "flickr"
 
 #
-CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
+CUDA_VISIBLE_DEVICES=0 python -m sqllava.eval.model_cap \
  --model_path ./checkpoints/path/to/ckpt/ \
   --model_base ./checkpoints/sharegpt4_pretrain\
  	--question-file ./playground/nocaps/out-domain_question.jsonl \
@@ -58,13 +58,13 @@ CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
 	--temperature 0 \
     --lora_pretrain ./checkpoints/path/to/ckpt/Vit-lora \
 
-python llava/eval/eval_image_caption.py \
+python sqllava/eval/eval_image_caption.py \
 	--answers-file ./output/nocaps/1200.jsonl\
 	--annotation path/to/nocaps/nocaps_val_out_domain.json\
 	--model_name "1200" \
 	--dataset "nocap_out_d"
 #
-CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
+CUDA_VISIBLE_DEVICES=0 python -m sqllava.eval.model_cap \
  --model_path ./checkpoints/path/to/ckpt \
    --model_base ./checkpoints/sharegpt4_pretrain\
 	--question-file ./playground/conceptual/question3.jsonl \
@@ -74,7 +74,7 @@ CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_cap \
 	--temperature 0 \
 	--lora_pretrain ./checkpoints/path/to/ckpt/Vit-lora \
 
-python llava/eval/eval_image_caption.py \
+python sqllava/eval/eval_image_caption.py \
 	--answers-file ./output/conceptual/1200.jsonl\
 	--QA ./playground/conceptual/question3.jsonl \
 	--model_name "1200"\
